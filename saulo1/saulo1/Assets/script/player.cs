255f821b149de127895a4029ee0af7e98cac14fb
italolopes85 <italo.josue@escolar.ifrn.edu.br>
2025-02-07 11:47:56 -0300

    finalizado

--- saulo1/saulo1/Assets/script/player.cs
+++ saulo1/saulo1/Assets/script/player.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class player : MonoBehaviour
+public class Player : MonoBehaviour
 {
     public float velocidade = 10f;
     public float focaPulo = 10f;
